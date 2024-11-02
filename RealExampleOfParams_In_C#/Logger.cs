1383ab9b8116fe9353d71541f8c50eee3285abc2
najeebcodeforpakistan <94035662+najeebcodeforpakistan@users.noreply.github.com>
2024-11-02 08:05:47 -0700

    Add project files.

---
+++ RealExampleOfParams_In_C#/Logger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealExampleOfParams_In_C_
+{
+    public class Logger
+    {
+        /*
+          Why params is Useful Here
+          In this logging scenario, params makes the code more adaptable:
+          You can log any number of details without creating separate overloads or explicitly defining an array.
+          The logging method is clean and consistent across different calls.
+          If no details are passed, the method still functions correctly, adding flexibility for simple log entries.
+          This structure is particularly useful for applications requiring dynamic data logging, like error reporting, 
+          transaction processing, or action tracing.
+        */
+        public void LogAction(string actionType, params string[] details)
+        {
+            Console.WriteLine($"Action Type:{actionType}");
+            if (details.Length > 0){
+                Console.WriteLine("Details:");
+                foreach (var detail in details)
+                {
+                    Console.WriteLine($"-{detail}");
+                }
+            }
+        }
+    }
+}
---
+++ RealExampleOfParams_In_C#/Program.cs
@@ -0,0 +1,23 @@
+﻿using RealExampleOfParams_In_C_;
+
+public class Program
+{
+    public static void Main()
+    {
+        Logger logger = new Logger();
+
+        //Log ction with not details
+        logger.LogAction("UserLogin");
+
+        //Log an action with some addtional details
+        logger.LogAction("FileUpload","FileName: report.pdf","Size:5MB","Status:Successfully");
+
+        //Log anaction with multiple details
+        logger.LogAction("DataProcessing",
+            "Records Processed:1000",
+            "Errors:None",
+            "Duration:5 seconds",
+            "Processed By:DataServices"
+            );
+    }
+}
